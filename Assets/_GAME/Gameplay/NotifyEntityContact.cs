a3439266c532c55a854730a73317f85c03204ba1
MrSuicideFish <malcolm259@gmail.com>
2025-07-31 22:30:42 -0400

    interactables and first pass puzzle 1

---
+++ Assets/_GAME/Gameplay/NotifyEntityContact.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NotifyEntityContact : MonoBehaviour
+{
+    [Serializable]
+    public class NotifEvent
+    {
+        public string EntityName;
+        public UnityEvent OnNotify;
+    }
+
+    [SerializeField] private bool m_destoryEncroacher = true;
+    [SerializeField] private UnityEvent m_onAllNotified;
+    [SerializeField] private NotifEvent[] m_notifEvents;
+    private byte m_notifCount = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PhysInteractable physInteractable = other.GetComponent<PhysInteractable>();
+        if (physInteractable != null)
+        {
+            bool found = false;
+            foreach (var notifEvent in m_notifEvents)
+            {
+                if (notifEvent.EntityName == physInteractable.gameObject.name)
+                {
+                    found = true;
+                    m_notifCount++;
+                    notifEvent.OnNotify?.Invoke();
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                if (m_notifCount == m_notifEvents.Length)
+                {
+                    m_onAllNotified?.Invoke();
+                }
+
+                if (m_destoryEncroacher)
+                {
+                    physInteractable.Destroy();    
+                }
+            }
+        }
+    }
+}
--- Assets/_GAME/Interactable/Interactable.cs
+++ Assets/_GAME/Interactable/Interactable.cs
@@ -1,11 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace _GAME
 {
     public class Interactable : MonoBehaviour
     {
+        public bool IsInteractable = true;
         public string DisplayName = "interactable";
+        
+        [TextArea(2,2)]
         public string Instruction = string.Empty;
+        
+        [TextArea(2,2)]
+        public string DisabledInstruction = string.Empty;
+        
+        public delegate void InteractableEventHandler();
+        public event InteractableEventHandler OnDestroyInvoked;
+
+        public UnityEvent OnBeginInteract;
+        public UnityEvent OnEndInteract;
+
+        public UnityEvent OnInteractEnabled;
+        public UnityEvent OnInteractDisabled;
         
         public virtual void BeginHover()
         {
@@ -16,14 +32,32 @@ namespace _GAME
         {
         }
 
-        public virtual void BeginInteract()
+        public virtual void BeginInteract(Player player)
         {
-            
+            OnBeginInteract?.Invoke();
         }
 
         public virtual void EndInteract()
         {
-            
+            OnEndInteract?.Invoke();
+        }
+
+        public void Destroy()
+        {
+            OnDestroyInvoked?.Invoke();
+            GameObject.Destroy(gameObject, 0.1f);
+        }
+
+        public void EnableInteractable()
+        {
+            IsInteractable = true;
+            OnInteractEnabled?.Invoke();
+        }
+        
+        public void DisableInteractable()
+        {
+            IsInteractable = false;
+            OnInteractDisabled?.Invoke();
         }
     }
 }
